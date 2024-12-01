f94d9d936bf87743fce3c6575fb2edeed8b23c8e
Noe Martinez <Noe.Martinez@ukuepa.com>
2024-11-30 20:54:39 -0600

    ACT4-EJERCICIO1-exp

--- Bubble Sort.php
+++ Bubble Sort.php
@@ -1,7 +1,11 @@
 
 <?php
-/*El algoritmo Bubble Sort compara elementos adyacentes en un array 
-y los intercambia si están en el orden incorrecto. 
+/*
+1.	Problema de Ordenar Lista con Bubble Sort: Escribe un programa que ordene una lista de números 
+de forma descendente utilizando el algoritmo Bubble Sort. La lista puede contener duplicados y valores negativos. 
+Asegúrate de manejar estos casos y muestra la lista antes y después de aplicar el algoritmo.
+
+El algoritmo Bubble Sort compara elementos adyacentes en un array y los intercambia si están en el orden incorrecto. 
 Esto se repite hasta que la lista esté ordenada.*/
 
 function bubbleSortDescending(&$array)
@@ -27,4 +31,10 @@ echo "Lista original: " . implode(", ", $numbers) . "\n";
 
 bubbleSortDescending($numbers);
 echo "Lista ordenada (descendente): " . implode(", ", $numbers) . "\n";
+
+/*Lista original: 5, -2, 8, -7, 3, 3, 0, -7
+  Lista ordenada (descendente): 8, 5, 3, 3, 0, -2, -7, -7
+ */
+
 ?>
+
